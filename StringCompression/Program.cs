100d94a58dcdfd4c1d19bb820b9f9b9c22fbb7f8
zmil44 <zachary.j.miller@outlook.com>
2018-10-17 13:38:59 -0400

    finished exercise

--- StringCompression/Program.cs
+++ StringCompression/Program.cs
@@ -10,6 +10,33 @@ namespace StringCompression
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Compress("aaabb"));
+            Console.WriteLine(Compress("aadffexcd"));
+            Console.WriteLine(Compress("zzmmpppp"));
+            Console.WriteLine(Compress("ssssssssddddddasdsadas"));
+            Console.WriteLine(Compress("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+            Console.WriteLine(Compress("abcdefghijklmnopqrstuvwxyz"));
+        }
+        public static string Compress(string input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = 1;
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    stringBuilder.Append(input[i-1]);
+                    stringBuilder.Append(count);
+                    count = 1;
+                }
+            }
+            stringBuilder.Append(input[input.Length - 1]);
+            stringBuilder.Append(count);
+            return stringBuilder.ToString().Length>=input.Length ?  input :  stringBuilder.ToString();
         }
     }
 }
